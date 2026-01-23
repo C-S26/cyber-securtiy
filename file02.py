b1053b39950843bf481f3af720dc63437f43c2df
C-S26 <pavanprakash0909@gmail.com>
2026-01-23 08:54:12 +0530

    removed cmd execution

--- file02.py
+++ file02.py
@@ -1,19 +1,15 @@
 import webbrowser
 import os
-import subprocess   #bandit
-
-ADMIN_PASSWORD = "SuperSecret123"
+#import subprocess   removed for cmd execution security reasons 
 
 def login():
     username = input("Enter Username: ")
     password = input("Enter Password: ")
 
-    if username == "admin" and password == ADMIN_PASSWORD:
+    if username == "admin" and password == "SuperSecret123":
         print("Access Granted! Welcome to the system.")
-
-        cmd = input("Enter system command: ")
-        subprocess.Popen(cmd, shell=True)
-
+        # cmd = input("Enter system command: ")
+        # subprocess.Popen(cmd, shell=True) removed for cmd execution security reasons
         open_patch_page()
     else:
         print("Access Denied.")
@@ -21,6 +17,5 @@ def login():
 def open_patch_page():
     file_path = os.path.abspath(r"E:\cybersecurity\patch_mgmt.html")
     webbrowser.open(f"file://{file_path}")
-    GITHUB_TOKEN = "ghp_abcdefghijklmnopqrstuvwxyz123456"
 
 login()
